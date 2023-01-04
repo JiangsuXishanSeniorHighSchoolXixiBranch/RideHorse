fcfc50ea93fdff2c48214b710710774949d7bf2e
够立小激 <84974878+Goulixiaoji@users.noreply.github.com>
2023-01-04 08:45:00 +0800

    Add files via upload

--- src/main/java/branch/xixi/ridehorse/common/item/WhipItem.java
+++ src/main/java/branch/xixi/ridehorse/common/item/WhipItem.java
@@ -8,6 +8,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
@@ -18,7 +19,7 @@ import net.minecraft.world.level.Level;
  */
 public class WhipItem extends Item {
     public WhipItem() {
-        super(new Properties().stacksTo(1).durability(114));
+        super(new Properties().stacksTo(1).durability(114).tab(CreativeModeTab.TAB_TOOLS));
     }
 
     @Override
